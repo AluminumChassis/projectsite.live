fa2a512754751184cfec06f5a3c96574e3bcf141
Ben <Aluminumchassis@gmail.com>
2019-05-15 18:19:13 -0700

    Working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-const url = "https://ahead-holly.glitch.me/"
+const url = "http://projectsiteback-env.d8mzdpwcvn.us-east-2.elasticbeanstalk.com/"
 const Http = new XMLHttpRequest();
 Http.open("GET", url);
 Http.send();
 var e
+suggestions = document.getElementById('suggestions')
+suggestion = document.getElementById('suggestion')
 Http.onreadystatechange = function() {
-	e=JSON.parse(this.responseText)
+	suggestions.innerHTML = ""
+	suggestion.innerHTML = ""
+	e=this.responseText.split(",")
+	e.pop()
+	console.log(this.responseText)
+	for (var i in e) {
+		d = document.createElement("div");
+		d.className = "clickable bar"
+		d.innerHTML = e[i]
+		document.getElementById("suggestions").appendChild(d)
+	}
+}
+function submit(){
+	Http.open("POST", url);
+	Http.send(suggestion.value);
 }
