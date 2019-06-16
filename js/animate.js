99b9343205ed6d217ae6d6587b90ee5aa64d2f46
Ben <Aluminumchassis@gmail.com>
2019-06-16 16:32:39 -0700

    Style update

---
+++ js/animate.js
@@ -0,0 +1,4 @@
+body = document.getElementsByTagName('body')[0]
+body.onscroll = function(evt) {
+	console.log("Scrolling")
+}
--- js/script.js
+++ js/script.js
@@ -1,61 +1,14 @@
-const url = "https://beans.projectsite.live/"
-const searchURL = "https://search-resorch-vwcs6cz6izfgw6tb6nwoz4grly.us-east-2.es.amazonaws.com/main/_search?q="
-const site = "projectsite.live/"
-const post = new XMLHttpRequest();
-const lookup = new XMLHttpRequest();
-post.open("GET", url);
-post.send();
-var e
-var search = true
-suggestions = document.getElementById('suggestions')
-suggestion = document.getElementById('suggestion')
-s = document.getElementById('search')
-post.onreadystatechange = function() {
-	suggestions.innerHTML = ""
-	e=this.responseText.split(",")
-	e.pop()
-	console.log(this.responseText)
-	for (var i in e) {
-		d = document.createElement("div");
-		d.className = "clickable bar"
-		d.innerText = e[i]
-		document.getElementById("suggestions").appendChild(d)
-	}
+dropDown = document.getElementById('dropDown')
+clickOut = document.getElementById('clickOut')
+login = document.getElementById('login')
+login.onclick = function () {
+	dropDown.style.top = "25vh"
+	clickOut.style.display = "block"
 }
-lookup.onreadystatechange = function() {
-	e=JSON.parse(this.responseText)
-	h = e.hits.hits
-	clearSearch()
-	for (var i in h) {
-		a = document.createElement("a");
-		a.href = h[i]._source.Id
-		a.innerText = h[i]._source.ProjectName
-		a.className = "clickable bar result"
-		document.getElementById("searchBar").appendChild(a)
-	}
+clickOut.onclick = function() {
+	dropDown.style.top = "-50vh"
+	clickOut.style.display = "none"
 }
-function submit(){
-	post.open("POST", url);
-	post.send(suggestion.value);
-	suggestion.value = ""
-}
-function clearSearch() {
-	results = document.getElementsByClassName("clickable bar result")
-	for (var i = results.length - 1; i >= 0; i--) {
-		results[i].parentNode.removeChild(results[i])
-	}
-}
-function searchProjects(){
-	search = false
-	setTimeout(
-		function(){
-			if(!search) {
-				lookup.open("GET", searchURL + s.value + "*");
-				console.log(searchURL + s.value)
-				lookup.send()
-			}
-			search = true
-		}
-	,200)
+function onSignIn(){
 	
 }
