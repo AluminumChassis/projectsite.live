b78a45cd079c466a91bb3efb605594a69eea42da
Ben <Aluminumchassis@gmail.com>
2019-06-22 20:17:32 -0700

    Nav loading

--- js/script.js
+++ js/script.js
@@ -40,5 +40,5 @@ fetch('/nav.html')
     return response.text();
   })
   .then(function(body) {
-    document.getElementsByTag("body")[0].innerHTML += body;
+  	document.getElementsByTagName("body")[0].innerHTML += body;
   });
